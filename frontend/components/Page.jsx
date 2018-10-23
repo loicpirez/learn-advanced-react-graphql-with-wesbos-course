24db44c875d5d05607972ba7748895a0cd1d7fcb
Loïc Pirez <loic@pirez.fr>
2018-10-23 12:33:22 +0800

    Lesson 5

--- frontend/components/Page.jsx
+++ frontend/components/Page.jsx
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
 import Header from '../components/Header'
 import Meta from '../components/Meta'
+import styled from 'styled-components'
+
+const MyButton = styled.button`
+  background: red;
+  font-size: ${
+    props => props.huge ? '100px' : '50px'
+  };
+  .poop {
+    font-size: 100px;
+  }
+`
+
 class Page extends Component {
   render() {
     return (
       <div>
-      <Meta />
-      <Header/>
+        <Meta />
+        <Header/>
+        <MyButton huge>
+          Click me
+          <span className="poop">💩</span>
+        </MyButton>
+        <MyButton>
+          Click me
+          <span className="poop">💩</span>
+        </MyButton>
         {this.props.children}
       </div>
     )
